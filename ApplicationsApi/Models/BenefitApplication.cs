40f1f1d3458132c5358b63464a80ff254b7a1f7b
Cevert90 <Unit_Alpha@hotmail.com>
2026-03-04 19:11:24 +0100

    Add BenefitApplication model, controller and SQLite database setup

---
+++ ApplicationsApi/Models/BenefitApplication.cs
@@ -0,0 +1,10 @@
+﻿namespace ApplicationsApi.Models;
+
+public class BenefitApplication
+{
+    public int Id { get; set; }
+    public string EmployeeId { get; set; } 
+    public int BenefitId { get; set; }
+    public string Status { get; set; } 
+    public DateTime CreatedDate { get; set; }
+}
