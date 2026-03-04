cb935d6f6711bb19df665ded60a6eddeff4f420a
Cevert90 <Unit_Alpha@hotmail.com>
2026-03-04 22:01:41 +0100

    Added DecisionDate & GetByEmployee endpoint

--- ApplicationsApi/Models/BenefitApplication.cs
+++ ApplicationsApi/Models/BenefitApplication.cs
@@ -1,10 +1,15 @@
-﻿namespace ApplicationsApi.Models;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ApplicationsApi.Models;
 
 public class BenefitApplication
 {
     public int Id { get; set; }
-    public string EmployeeId { get; set; } 
+    [MaxLength(50)]
+    public required string EmployeeId { get; set; } 
     public int BenefitId { get; set; }
-    public string Status { get; set; } 
+    [MaxLength(50)]
+    public required string Status { get; set; } 
     public DateTime CreatedDate { get; set; }
+    public DateTime? DecisionDate  { get; set; }
 }
