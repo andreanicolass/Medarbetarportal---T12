1729e1c9bb4a7dc571c7fb13090432e83582272d
Cevert90 <Unit_Alpha@hotmail.com>
2026-03-04 19:11:24 +0100

    Add BenefitApplication model, controller and SQLite database setup

---
+++ ApplicationsApi/Controllers/ApplicationsController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ApplicationsApi.Models;
+
+namespace ApplicationsApi.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class ApplicationsController : ControllerBase
+{
+    private static List<BenefitApplication> _applications = new();
+
+    // GET api/applications
+    [HttpGet]
+    public IActionResult GetAll()
+    {
+        return Ok(_applications);
+    }
+
+    // POST api/applications
+    [HttpPost]
+    public IActionResult Create(BenefitApplication application)
+    {
+        application.Id = _applications.Count + 1;
+        application.CreatedDate = DateTime.Now;
+        application.Status = "Pending";
+        _applications.Add(application);
+        return CreatedAtAction(nameof(GetAll), application);
+    }
+}
