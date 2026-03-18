1b27ab4803c0609353ffcc563c81439c4d4da042
William <wist0008@student.hv.se>
2026-03-18 17:14:46 +0100

    William: API helt klart för frontend med databas, Scalar och CORS

---
+++ Benefits-Api/Controllers/BenefitsController.cs
@@ -0,0 +1,35 @@
+﻿using Benefits_Api.Data;
+using Benefits_Api.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Benefits_Api.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class BenefitsController : ControllerBase
+{
+    private readonly AppDbContext _context;
+
+    public BenefitsController(AppDbContext context)
+    {
+        _context = context;
+    }
+
+    // GET: api/Benefits (Hämtar alla förmåner)
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<Benefit>>> GetBenefits()
+    {
+        return await _context.Benefits.Include(b => b.Category).ToListAsync();
+    }
+
+    // POST: api/Benefits (Lägger till en ny förmån)
+    [HttpPost]
+    public async Task<ActionResult<Benefit>> PostBenefit(Benefit benefit)
+    {
+        _context.Benefits.Add(benefit);
+        await _context.SaveChangesAsync();
+
+        return CreatedAtAction(nameof(GetBenefits), new { id = benefit.Id }, benefit);
+    }
+}
--- Benefits-Api/Program.cs
+++ Benefits-Api/Program.cs
@@ -1,20 +1,40 @@
+using Benefits_Api.Data;
+using Microsoft.EntityFrameworkCore;
+using Scalar.AspNetCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+
+// KONFIGURATION FÖR DATABAS (SQLite)
+// Här kopplas AppDbContext ihop med SQLite-motorn
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlite("Data Source=benefits.db"));
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll",
+        policy => policy.AllowAnyOrigin()
+            .AllowAnyMethod()
+            .AllowAnyHeader());
+});
+
 builder.Services.AddOpenApi();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+    app.MapScalarApiReference();
 }
 
 app.UseHttpsRedirection();
+
+app.UseCors("AllowAll");
 
 app.UseAuthorization();
 
